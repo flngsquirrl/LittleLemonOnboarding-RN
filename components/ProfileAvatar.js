f30b88fdeceeb2b09c958cc26a13fdb3f8ef25df
flngsquirrl <y.charniak@gmail.com>
2024-03-18 22:40:45 +0100

    Set square as aspect for ImagePicker
    For Android only; in iOS is is a square by default

--- components/ProfileAvatar.js
+++ components/ProfileAvatar.js
@@ -14,7 +14,7 @@ const ProfileAvatar = () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
