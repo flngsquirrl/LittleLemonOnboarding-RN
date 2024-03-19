9cd2508e1102d6212c95fe2217f98da7f0ae9f96
flngsquirrl <y.charniak@gmail.com>
2024-03-20 00:37:34 +0100

    Fix directory existence check

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -2,13 +2,11 @@ import * as FileSystem from "expo-file-system";
 
 export async function prepareDirectory(directoryPath) {
   const targetDirectory = await FileSystem.getInfoAsync(directoryPath);
-  if (!targetDirectory.isDirectory) {
+  if (!targetDirectory.exists) {
     try {
-      await FileSystem.makeDirectoryAsync(directoryPath, {
-        intermediates: true,
-      });
+      await FileSystem.makeDirectoryAsync(directoryPath);
     } catch (error) {
-      console.info("Error while making directory", error);
+      console.error("Error while making directory", error);
     }
   }
 }
