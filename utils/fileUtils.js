ace322ea11ea9e5a4b2c943e6fb35b235d665d44
flngsquirrl <y.charniak@gmail.com>
2024-03-23 15:47:43 +0100

    Save avatar file with its original name
    If the file is saved with some fixed name, the Image component won't get updated when the file is updated, cause the change is hidden from it

--- utils/fileUtils.js
+++ utils/fileUtils.js
@@ -4,9 +4,15 @@ export async function prepareDirectory(directoryPath) {
   const targetDirectory = await FileSystem.getInfoAsync(directoryPath);
   if (!targetDirectory.exists) {
     try {
-      await FileSystem.makeDirectoryAsync(directoryPath);
+      await FileSystem.makeDirectoryAsync(directoryPath, {
+        intermediates: true,
+      });
     } catch (error) {
       console.error("Error while making directory", error);
     }
   }
+}
+
+export function getFileName(fileUri) {
+  return fileUri.slice(fileUri.lastIndexOf("/") + 1);
 }
